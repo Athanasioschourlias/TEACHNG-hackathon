1a056c7bb1ffe763d26501e2f7f39ef833bb57f9
Athanasioschourlias <thanos.chourlias@gmail.com>
2023-06-30 15:08:33 +0300

    seting up basic example

--- modules/tsd_module.py
+++ modules/tsd_module.py
@@ -4,6 +4,7 @@ from base.node import TEACHINGNode
 from .base_module import LearningModule
 
 import numpy as np
+import darknet
 from keras.applications.vgg16 import preprocess_input, decode_predictions
 from keras.applications.vgg16 import VGG16
 
@@ -17,37 +18,40 @@ class TSDModule(LearningModule):
     @TEACHINGNode(produce=False, consume=True)
     def __call__(self, input_fn):
         for msg in input_fn:
-            np_img = np.asarray(eval(msg.body["img"]), dtype='uint8').reshape(224, 224, 3)
+            frame = np.asarray(eval(msg.body["img"]), dtype='uint8').reshape(224, 224, 3)
             print("image received!")
 
             print("HELLOOO")
-            # Load the pre-trained VGG16 model
-            model = VGG16(weights='imagenet')
 
-            # Preprocess the frame for VGG16 model
-            frame = preprocess_input(np_img)
+            # Load the YOLOv4-Tiny model configuration and weights
+            config_file = 'cfg/yolov4-tiny.cfg'
+            weight_file = 'weights/yolov4-tiny.weights'
 
-            # Expand dimensions to match VGG16 input shape
-            frame = np.expand_dims(frame, axis=0)
+            # Load the COCO class names
+            class_names = []
+            with open('data/coco.names', 'r') as f:
+                class_names = [line.strip() for line in f.readlines()]
 
-            # Perform classification
-            preds = model.predict(frame)
-            predictions = decode_predictions(preds, top=3)[0]
+            # Load the YOLOv4-Tiny model
+            network, _ = darknet.load_network(config_file, weight_file, batch_size=1)
 
-            print("predictions", predictions)
+            # Convert the frame to a darknet image
+            darknet_image = darknet.make_image(frame.shape[1], frame.shape[0], 3)
 
-            # # Display the top predictions
-            # for _, label, probability in predictions:
-            #     cv2.putText(frame, f"{label}: {probability:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            #     # Display the resulting frame
-            #     cv2.imshow('Video Classification', frame)
-            #     if cv2.waitKey(1) & 0xFF == ord('q'):
-            #         break
+            darknet.copy_image_from_bytes(darknet_image, frame.tobytes())
+
+            # Perform object detection
+            detections = darknet.detect_image(network, class_names, darknet_image)
+
+            # Draw bounding boxes and labels on the frame
+            darknet.print_detections(detections)
+            darknet.draw_boxes(detections, frame)
 
 
-            # img_batch = np.expand_dims(np_img, 0)
-            # pred = self.model.predict(preprocess_input(img_batch))
-            # print("Predictions: ", decode_predictions(pred))
+
+            # Display the resulting frame
+            # frame_resized = cv2.resize(frame_rgb, (frame.shape[1], frame.shape[0]))
+            # cv2.imshow('Object Detection', frame_resized)
 
     def _build(self):
         self.model = keras.applications.MobileNetV2(weights='imagenet', include_top=True)
