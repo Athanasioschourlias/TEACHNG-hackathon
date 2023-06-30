cbc684dd1eb4f1a7f6b9a499329e778836d4de79
Athanasioschourlias <thanos.chourlias@gmail.com>
2023-06-30 13:14:12 +0300

    seting up basic example

--- federated/aggregation/aggregators.py
+++
@@ -1,8 +0,0 @@
-from .fedavg_aggregator import FedAvgAggregator
-
-
-def get_aggregator(name: str):
-    if name == 'fedavg':
-        return FedAvgAggregator()
-    else:
-        raise ValueError(f"Parameter {name} is invalid.")
