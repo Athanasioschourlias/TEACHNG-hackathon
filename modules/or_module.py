a7b7dfff1c85b34860f0f9977b35e73a4db90e4c
Athanasioschourlias <thanos.chourlias@gmail.com>
2023-06-30 13:26:26 +0300

    seting up basic example

--- modules/or_module.py
+++ modules/or_module.py
@@ -19,9 +19,10 @@ class ORModule(LearningModule):
             np_img = np.asarray(eval(msg.body["img"]), dtype='uint8').reshape(224, 224, 3)
             print("image received!")
 
-            img_batch = np.expand_dims(np_img, 0)
-            pred = self.model.predict(preprocess_input(img_batch))
-            print("Predictions: ", decode_predictions(pred))
+            print("HELLOOO")
+            # img_batch = np.expand_dims(np_img, 0)
+            # pred = self.model.predict(preprocess_input(img_batch))
+            # print("Predictions: ", decode_predictions(pred))
 
     def _build(self):
         self.model = keras.applications.MobileNetV2(weights='imagenet', include_top=True)
